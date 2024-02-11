8c665ab0b32879c3aed419b08ae2fb2df0136616
TiagoBatixta <tiagobatista-slv@outlook.com>
2024-02-11 01:55:41 -0300

    Organo 1.2

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import Banner from './componentes/Banner/Banner';
+import CampoTexto from './componentes/CampoTexto';
 
 function App() {
   return (
     <div className="App">
-      <Banner/>
+      <Banner />
+      <CampoTexto label="Nome" placeholder="Digite o seu Nome"/>
+      <CampoTexto label="Cargo" placeholder="Digite o seu Cargo"/>
+      <CampoTexto label="Imagem"  placeholder="Digite o endereço da imagem "/>
     </div>
   );
 }
